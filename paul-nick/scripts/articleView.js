68faaf456991d914493a733b377bc1dcfc92ea17
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-06-07 13:43:00 -0700

    Complete, no stretch goals.

--- paul-nick/scripts/articleView.js
+++ paul-nick/scripts/articleView.js
@@ -77,19 +77,18 @@ articleView.handleMainNav = function () {
   // TODO: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  $('nav').on('click', '.tab', function (event) {
-    event.preventDefault();
-    if ($(this).val()) {
-      $('article').hide();
-      // $(`article[tab-content=${$(this).val()}]`).fadeIn('500');
-      $('article[tab-content =' + $(this).val() + ']').fadeIn('500');
-    } else {
-      $('article').show();
-      // .hasClass('template')
-    }
-  });
+  $('.tab[data-content="articles"]').on("click", function () {
+    $('.tab-content').hide();
+    $('.tab-content:first').fadeIn(500);
+  })
+  $('.tab[data-content="about"]').on("click", function () {
+    $('.tab-content').hide();
+    $('.tab-content:last').fadeIn(500);
+  })
+  // .hasClass('template')
+
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  $('nav .tab:first').click();
+
 };
 
 articleView.setTeasers = function () {
@@ -98,16 +97,19 @@ articleView.setTeasers = function () {
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
-  // $('a[.read-on]').on('click', function () {
-  //   $('article-body').show();
-  // })
+  $('.read-on').on('click', function (event) {
+    event.preventDefault();
+    $(this).parent().find("*").fadeIn(500);
+    $(this).hide();
+    $('article-body').show();
+  })
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function () {
   articleView.populateFilters();
-  articleView.handleAuthorFilter(); //Not working//
+  articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
-  articleView.handleMainNav(); //Not working//
-  articleView.setTeasers(); //Not working//
+  articleView.handleMainNav(); 
+  articleView.setTeasers(); 
 })
